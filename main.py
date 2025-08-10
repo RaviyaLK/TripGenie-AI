a18e8d70d30927665a36e029a2cd2b5625626f7b
RaviyaLK <103655504+RaviyaLK@users.noreply.github.com>
2025-08-10 21:51:23 +0530

    Minor changes

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ app.add_middleware(
     allow_headers=["*"],  
 )
 
-
-
-# Include the chat router
 app.include_router(chat_router, prefix="/api")
 
 @app.get("/")
--- schemas.py
+++ schemas.py
@@ -2,7 +2,6 @@
 from pydantic import BaseModel, Field
 from typing import List
 
-# --- Chat Schemas ---
 
 class ChatPart(BaseModel):
     text: str
